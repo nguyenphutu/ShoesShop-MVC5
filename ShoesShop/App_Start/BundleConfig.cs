a523e96bdd2051b3dcde9db090adc0bc5f9d53aa
nguyentu1995 <nguyentudb1995@gmail.com>
2018-04-04 16:54:52 +0700

    Add structure mvc .net to shoes shop project

---
+++ ShoesShop/App_Start/BundleConfig.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace ShoesShop.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/scripts/jquery-2.1.4.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap3").Include(
+                "~/scripts/bootstrap-3.1.1.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery-easing").Include(
+                "~/scripts/easing.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                "~/scripts/modernizr-2.6.2.min.js",
+                "~/scripts/classie.js",
+                "~/scripts/demo1.js",
+                "~/scripts/minicart.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/search").Include(
+                "~/scripts/search.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/responsiveslides").Include(
+                "~/scripts/responsiveslides.min.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/Content/bootstrap").Include(
+            "~/Content/Css/bootstrap.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/Content/font-awesome").Include(
+                "~/Content/Css/font-awesome.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/Content/css").Include(
+                "~/Content/Css/shop.css",
+                "~/Content/Css/style7.css",
+                "~/Content/Css/style.css"
+                ));           
+
+        }
+    }
+}
---
+++ ShoesShop/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace ShoesShop.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Admin_default",
+                "Admin/{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "ShoesShop.Areas.Admin.Controllers" }
+            );
+        }
+    }
+}
